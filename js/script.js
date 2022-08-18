269de88b7130d42ec0e385434af81592f9ece6b1
Kevin2211 <trinh_thanh42@yahoo.com>
2022-08-18 17:21:43 -0500

    add functions to all buttons (final commit)

--- js/script.js
+++ js/script.js
@@ -1,29 +1,50 @@
-const add = (x,y) => x + y;
-const sub = (x,y) => x - y;
-const multiply = (x,y) => x * y;
-const divide = (x,y) => x / y;
-
-const total = 0;
-
-// function numButtonClicked (num) {
-//     console.log(num);
-// }
 
 const numButtonDivs = document.getElementsByClassName("numbers");
-const operations = document.querySelector('.operators');
+const operations = document.querySelector('.operators').children;
 const display = document.getElementById('input');
-const result = 0;
+const equalButton = document.getElementById('result');
+const clearButton = document.getElementById('clear');
+
+
+let result = 0;
+const operators = ["+","-","/","*"]
+
 
 
 for (let j = 0; j < numButtonDivs.length; j++) {
     for (let i = 0; i < numButtonDivs[j].children.length; i++) {
-        console.log(numButtonDivs[j].children[i].addEventListener('click', (event) => {
+            numButtonDivs[j].children[i].addEventListener('click', (event) => {
+            if(result != 0){
+                display.textContent = '';
+                result =0;
+            }
+            
             display.textContent += event.target.textContent;
-        }))  
+        }) 
     } 
 }
 
-console.log(operations);
+for (let i = 0; i < operations.length; i++) {
+    operations[i].addEventListener('click', (e) => {
+        if(display.textContent != "" && operators.includes(display.textContent.slice(-1)) == false){
+            display.textContent += e.target.textContent;        
+        }
+    })
+}
+
+
+equalButton.addEventListener('click', () => {
+    if(operators.includes(display.textContent.slice(-1)) == true){
+        display.textContent = display.textContent.slice(0, -1);
+    }
+    result = eval(display.textContent);   
+    display.textContent = result;
+
+})
+
+clearButton.addEventListener('click', () => {
+    display.textContent = '';
+})
 
 
 
